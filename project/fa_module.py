f29aac675a855d74936703eedc31fbc94c2d952a
Lesh79 <Temerlan.axmetov.75@mail.ru>
2024-09-13 20:48:16 +0300

    feat: add finite automata module and test for it

---
+++ project/fa_module.py
@@ -0,0 +1,32 @@
+from typing import Set
+
+from networkx import MultiDiGraph
+from pyformlang.finite_automaton import (
+    DeterministicFiniteAutomaton,
+    NondeterministicFiniteAutomaton,
+)
+from pyformlang.regular_expression import Regex
+
+
+def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton:
+    return Regex(regex).to_epsilon_nfa().to_deterministic()
+
+
+def graph_to_nfa(
+    graph: MultiDiGraph, start_states: Set[int] = None, final_states: Set[int] = None
+) -> NondeterministicFiniteAutomaton:
+    nfa = NondeterministicFiniteAutomaton.from_networkx(graph)
+
+    if start_states is None:
+        start_states = graph.nodes
+
+    if final_states is None:
+        final_states = graph.nodes
+
+    for state in start_states:
+        nfa.add_start_state(state)
+
+    for state in final_states:
+        nfa.add_final_state(state)
+
+    return nfa
---
+++ tests/test_build_dfa.py
@@ -0,0 +1,37 @@
+from pyformlang.finite_automaton import NondeterministicFiniteAutomaton
+
+from project.fa_module import regex_to_dfa, graph_to_nfa
+from project.graph_module import load_graph
+
+
+class TestDFA:
+    def test_regex_to_dfa(self):
+        regex = "xy* (x | y*)"
+        dfa = regex_to_dfa(regex)
+
+        excepted = NondeterministicFiniteAutomaton()
+        excepted.add_start_state(0)
+        excepted.add_final_state(0)
+        excepted.add_final_state(1)
+        excepted.add_final_state(2)
+        excepted.add_transitions([(0, "xy", 0), (0, "y", 1), (1, "y", 1), (0, "x", 2)])
+        assert excepted.is_equivalent_to(dfa)
+
+    def test_build_dfa_from_regex(self):
+        regex = "xy* (x | y*) | ab (x | y*) | (x | a*) (x | y*)"
+        dfa = regex_to_dfa(regex)
+        assert dfa.is_deterministic()
+        dfa_minimize = dfa.minimize()
+        assert dfa.is_equivalent_to(dfa_minimize)
+
+    def test_build_from_loaded_graph(self):
+        print("build dfa from loaded graph")
+
+        graph = load_graph("wc")
+        nfa = graph_to_nfa(graph)
+
+        excepted_graph_info = 332
+
+        assert len(nfa.states) == excepted_graph_info
+        assert len(nfa.final_states) == excepted_graph_info
+        assert len(nfa.start_states) == excepted_graph_info
